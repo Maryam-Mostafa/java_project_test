8c8bfe0d4e41aa6fdb7a2141af041a27bb0fc32a
maryam <maryam1998.mostafa@gmail.com>
2022-03-05 18:31:01 +0200

    master second

--- src/maryam.java
+++ src/maryam.java
@@ -1,2 +1,12 @@
 public class maryam {
+
+    void fun1()
+    {
+        System.out.println("hello first try ");
+    }
+    void fun2()
+    {
+        System.out.println("hello second try ");
+    }
 }
+
